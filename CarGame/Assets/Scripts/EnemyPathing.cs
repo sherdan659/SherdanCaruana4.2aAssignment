00673a4c0b97a46fb53052305d12b1d3c19af01c
sherdan659 <sherdan659@gmail.com>
2021-01-06 02:22:36 +0100

    Created an Obstacle Wave Scriptable file

---
+++ CarGame/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] ObstacleWaveScript obstacleWaveScript;
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        
+        transform.position = waypoints[waypointIndex].transform.position;
+        waypoints = obstacleWaveScript.GetPathPrefab();
+    }
+
+
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    void EnemyMove()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            targetPosition.z = 0f;
+
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
---
+++ CarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float padding = 1f;
+    float XMin;
+    float XMax;
+    float YMin;
+    float YMax;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ViewPortToWorldPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime;
+        var newX = transform.position.x + deltaX;
+        transform.position = new Vector2(newX, transform.position.y);
+        
+    }
+
+    private void ViewPortToWorldPoint()
+    {
+        Camera gameCamera = Camera.main;
+        
+
+        XMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        XMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        YMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        YMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+
+
+
+    }
+}
